0bd93055bbe4ea0471f96907904902fc8990e3b4
Thanh Mi Nguyen Thanh <thanhmi1111@live.com>
2021-04-08 22:39:02 +0800

    use github action to set up cicd and deploy to heroku

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,21 @@ import (
 	"github/meoconbatu/subnet"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	http.Handle("/", http.FileServer(http.Dir("./web/view/build")))
+
 	http.HandleFunc("/subnet", subnet.SubnetHandler)
 	http.HandleFunc("/divide", subnet.DivideHandler)
 	http.HandleFunc("/search", subnet.SearchHandler)
 	http.HandleFunc("/download", subnet.DownloadHandler)
 	http.HandleFunc("/upload", subnet.UploadHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
