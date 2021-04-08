e15378a484567b20b940f1409fba8e2cc14133aa
Thanh Mi Nguyen Thanh <thanhmi1111@live.com>
2021-04-08 22:37:26 +0800

    serve react

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	http.Handle("/", http.FileServer(http.Dir("./web/view/build")))
 	http.HandleFunc("/subnet", subnet.SubnetHandler)
 	http.HandleFunc("/divide", subnet.DivideHandler)
 	http.HandleFunc("/search", subnet.SearchHandler)
