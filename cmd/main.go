9a79986e2f1ffdb22cb40e542d5ad3099f765783
Thanh Mi Nguyen Thanh <thanhmi1111@live.com>
2021-04-09 23:23:14 +0800

    delete debug code

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"github/meoconbatu/subnet"
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
 func main() {
-	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		fmt.Println(path, info.Size())
-		return nil
-	})
-	if err != nil {
-		log.Println(err)
-	}
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
